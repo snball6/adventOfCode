daf47ce951ca67ddaf2f156d4e5bc81e7454879c
Brooklyn Dev <brooklyn@menloinnovations.com>
2021-08-19 20:34:59 -0400

    Day 1 part 1 complete

--- src/day1.js
+++ src/day1.js
@@ -1,3 +1,11 @@
 function fuelByMass(mass) {
     return Math.floor(mass/3)-2;
 }
+
+function getTotalFuel(modules){
+    let totalFuel = 0;
+    for(let i = 0; i<modules.length; i++){
+        totalFuel+=fuelByMass(modules[i]);
+    }
+    return totalFuel;
+}
