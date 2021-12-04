76e6e362a033e4803f66f506dcd14ca5cb2dc5e9
Sarah Ball <brooklyn@menloinnovations.com>
2021-12-04 10:56:15 -0500

    Day 4 part 2. Removing some console logs and accidentally commented out sample puzzle input. Did less granularity of testing for sample data of part 2 and went straight for trying to hit the final score.

--- spec/2021/day4.spec.js
+++ spec/2021/day4.spec.js
@@ -65,13 +65,19 @@ describe("day4", () => {
             expect(getWinningScoreOfFirstBoardToWin(sampleData)).toEqual(4512);
         });
 
-        it("Should find final score_[uzzle input]", () => {
-            expect(getWinningScoreOfFirstBoardToWin(puzzleInput)).toEqual(4512);
+        it("Should find final score_puzzle input", () => {
+            expect(getWinningScoreOfFirstBoardToWin(puzzleInput)).toEqual(8580);
         });
     });
 
     describe("Part 2", () => {
+        it("Should find final score of last board to win_sample data", () => {
+            expect(getWinningScoreOfLastBoardToWin(sampleData)).toEqual(1924);
+        });
 
+        it("Should find final score of last board to win_puzzle input", () => {
+            expect(getWinningScoreOfLastBoardToWin(puzzleInput)).toEqual(9576);
+        });
     });
 
     //Manually parsed boards to arrays using the steps:
@@ -84,7 +90,7 @@ describe("day4", () => {
     // add outer array
 
     let sampleData = {
-        drawnNumbers: [7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24],//, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19, 3, 26, 1],
+        drawnNumbers: [7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19, 3, 26, 1],
         boards: [[[22, 13, 17, 11, 0], [8, 2, 23, 4, 24], [21, 9, 14, 16, 7], [6, 10, 3, 18, 5], [1, 12, 20, 15, 19]],
         [[3, 15, 0, 2, 22], [9, 18, 13, 17, 5], [19, 8, 7, 25, 23], [20, 11, 10, 24, 4], [14, 21, 16, 12, 6]],
         [[14, 21, 17, 24, 4], [10, 16, 15, 9, 19], [18, 8, 23, 26, 20], [22, 11, 13, 6, 5], [2, 0, 12, 3, 7]]]
--- src/2021/day4.js
+++ src/2021/day4.js
@@ -18,13 +18,13 @@ class Board {
 
     constructor(arrayOfBoard) {
         //Sample and test input are 5 by 5 boards. Hardcoding
-        for(let row = 0; row<5; row++){
+        for (let row = 0; row < 5; row++) {
             let boardNumberRow = [];
-            for(let column = 0; column <5; column++){
+            for (let column = 0; column < 5; column++) {
                 let gridNum = arrayOfBoard[row][column];
                 let newNum = new BoardNumber(arrayOfBoard[row][column]);
                 boardNumberRow.push(newNum);
-                this.dictionaryOfNumbers[gridNum]= {
+                this.dictionaryOfNumbers[gridNum] = {
                     boardNumber: newNum,
                     row: row,
                     column: column
@@ -35,30 +35,28 @@ class Board {
     }
 
     markNumber(number) {
-        if(number.toString() in this.dictionaryOfNumbers){
+        if (number.toString() in this.dictionaryOfNumbers) {
             let boardNumberData = this.dictionaryOfNumbers[number];
             boardNumberData.boardNumber.markNumber();
-            this.rowCounts[boardNumberData.row] ++;
-            this.columnCounts[boardNumberData.column] ++;
+            this.rowCounts[boardNumberData.row]++;
+            this.columnCounts[boardNumberData.column]++;
         }
         return this.checkForFullRowOrColumn();
     }
 
     getSumOfUnmarked() {
-        console.log("Totalling")
         let total = 0;
-        for (let num in this.dictionaryOfNumbers){
-            if(!this.dictionaryOfNumbers[num].boardNumber.marked){
-                total+=this.dictionaryOfNumbers[num].boardNumber.number;
+        for (let num in this.dictionaryOfNumbers) {
+            if (!this.dictionaryOfNumbers[num].boardNumber.marked) {
+                total += this.dictionaryOfNumbers[num].boardNumber.number;
             }
         }
         return total;
     }
 
-    checkForFullRowOrColumn(){
-        for(let i = 0; i<5; i++){
-            if(this.rowCounts[i]>=5 || this.columnCounts[i] >=5){
-                console.log("YES");
+    checkForFullRowOrColumn() {
+        for (let i = 0; i < 5; i++) {
+            if (this.rowCounts[i] >= 5 || this.columnCounts[i] >= 5) {
                 return true;
             }
         }
@@ -78,20 +76,35 @@ class BoardNumber {
     }
 }
 
-function getWinningScoreOfFirstBoardToWin(input){
+function getWinningScoreOfFirstBoardToWin(input) {
     let drawnNumbersArray = input['drawnNumbers'];
     let boards = [];
-    for(let i = 0; i<input['boards'].length; i++){
+    for (let i = 0; i < input['boards'].length; i++) {
         boards.push(new Board(input['boards'][i]))
     }
-    console.log(boards);
-    for(let numberDrawnIndex = 0; numberDrawnIndex < drawnNumbersArray.length; numberDrawnIndex++){
-        for(let boardIndex = 0; boardIndex < boards.length; boardIndex++){
-            if(boards[boardIndex].markNumber(drawnNumbersArray[numberDrawnIndex])){
-                console.log("HERE TOO");
+    for (let numberDrawnIndex = 0; numberDrawnIndex < drawnNumbersArray.length; numberDrawnIndex++) {
+        for (let boardIndex = 0; boardIndex < boards.length; boardIndex++) {
+            if (boards[boardIndex].markNumber(drawnNumbersArray[numberDrawnIndex])) {
                 return boards[boardIndex].getSumOfUnmarked() * drawnNumbersArray[numberDrawnIndex];
             };
-        }   
+        }
     }
-    console.log(boards);
+}
+
+function getWinningScoreOfLastBoardToWin(input) {
+    let drawnNumbersArray = input['drawnNumbers'];
+    let boards = [];
+    for (let i = 0; i < input['boards'].length; i++) {
+        boards.push(new Board(input['boards'][i]))
+    }
+    for (let numberDrawnIndex = 0; numberDrawnIndex < drawnNumbersArray.length; numberDrawnIndex++) {
+        if (boards.length != 1) {
+            //if we are not yet to the last board, mark the number and keep filtering out winners
+            boards = boards.filter(board => !board.markNumber(drawnNumbersArray[numberDrawnIndex]));
+        } else {
+            if(boards[0].markNumber(drawnNumbersArray[numberDrawnIndex])){
+                return boards[0].getSumOfUnmarked() * drawnNumbersArray[numberDrawnIndex];
+            }
+        }
+    }
 }
