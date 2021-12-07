9546229d906ec3ad23984520991fef02fdd69539
Sarah Ball <brooklyn@menloinnovations.com>
2021-12-07 07:37:25 -0500

    Countdown clock working! And now I realize it's counting up...not going to bother renaming variables.

--- animateLeaderBoard/script.js
+++ animateLeaderBoard/script.js
@@ -14,10 +14,46 @@
 //         }
 //     }
 // }; 
-let countDownTime = new Date(2021, 12, 01, 01, 00);
+let countDownTime = new Date('December 1, 2021 01:00:00');
+let countDownStop = new Date('December 8, 2021 01:00:00')
+console.log(countDownTime);
 let countdown = setInterval(function() {
     //NOT WORKING YET...
-    countDownTime.setHours(countDownTime.getHours()+1);
-    document.getElementById("countdown").innerHTML = countDownTime.getFullYear() + "." + countDownTime.getMonth() + "." + countDownTime.getDay() + " " + countDownTime.getHours() + ":" + countDownTime.getMinutes();
+    if(countDownTime < countDownStop)
+    countDownTime = dateAdd(countDownTime, 'minute', 1);
+    document.getElementById("countdown").innerHTML = customDateFormat(countDownTime);
 
-}, 1000);
+}, 1);
+
+function customDateFormat(date){// because of stupid 0 indexed months
+    return date.getFullYear() + "." + (date.getMonth()+1).toString().padStart(2, '0') + "." + date.getDate().toString().padStart(2, '0') + " " + date.getHours().toString().padStart(2, '0') + ":" + (date.getMinutes().toString().padStart(2, '0'));
+
+}
+
+/**
+ * Adds time to a date. Modelled after MySQL DATE_ADD function.
+ * Example: dateAdd(new Date(), 'minute', 30)  //returns 30 minutes from now.
+ * https://stackoverflow.com/a/1214753/18511
+ * 
+ * @param date  Date to start with
+ * @param interval  One of: year, quarter, month, week, day, hour, minute, second
+ * @param units  Number of units of the given interval to add.
+ */
+function dateAdd(date, interval, units) {
+    if(!(date instanceof Date))
+      return undefined;
+    var ret = new Date(date); //don't change original date
+    var checkRollover = function() { if(ret.getDate() != date.getDate()) ret.setDate(0);};
+    switch(String(interval).toLowerCase()) {
+      case 'year'   :  ret.setFullYear(ret.getFullYear() + units); checkRollover();  break;
+      case 'quarter':  ret.setMonth(ret.getMonth() + 3*units); checkRollover();  break;
+      case 'month'  :  ret.setMonth(ret.getMonth() + units); checkRollover();  break;
+      case 'week'   :  ret.setDate(ret.getDate() + 7*units);  break;
+      case 'day'    :  ret.setDate(ret.getDate() + units);  break;
+      case 'hour'   :  ret.setTime(ret.getTime() + units*3600000);  break;
+      case 'minute' :  ret.setTime(ret.getTime() + units*60000); break;
+      case 'second' :  ret.setTime(ret.getTime() + units*1000);  break;
+      default       :  ret = undefined;  break;
+    }
+    return ret;
+  }
