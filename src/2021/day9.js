1ebf954c505233c76904898d4bb5eefdad05d85f
Sarah Ball <brooklyn@menloinnovations.com>
2021-12-09 07:21:01 -0500

    Cleaning up console logs and inaccurate comments

--- src/2021/day9.js
+++ src/2021/day9.js
@@ -6,34 +6,28 @@ function findLowestPoints(input) {
         //for each string
         for (let j = 0; j < input[i].length; j++) {
             let lowCheck = true;
-            tempCurrent = input[i][j]; //might need to parse int here, but I think I can get away with it
+            tempCurrent = input[i][j];
 
-            let logThisLoop = true;
-            if (logThisLoop) console.log("next:", tempCurrent);
             if (notTopRow(i)) {
                 if (!isLessThan(tempCurrent,input[i - 1][j])) {
-                    if (logThisLoop) console.log("Top is lower")
                     lowCheck = false;
                 };
             }
 
             if (notLeftEdge(j)) {
                 if (!isLessThan(tempCurrent, input[i][j - 1])) {
-                    if (logThisLoop) console.log("left is lower")
                     lowCheck = false;
                 };
             }
 
             if (notBottom(i, input)) {
                 if (!isLessThan(tempCurrent, input[i + 1][j])) {
-                    if (logThisLoop) console.log("bottom is lower")
                     lowCheck = false;
                 };
             }
 
             if (notRightEdge(j, input)) {
                 if (!isLessThan(tempCurrent, input[i][j + 1])) {
-                    if (logThisLoop) console.log("right is lower")
                     lowCheck = false;
                 };
             }
@@ -43,7 +37,6 @@ function findLowestPoints(input) {
             }
         }
     }
-    console.log(lowest);
     return lowest;
 }
 
