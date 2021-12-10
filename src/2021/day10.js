ea5e22efc660746dbc9d092e726ebe8679341f45
Sarah Ball <brooklyn@menloinnovations.com>
2021-12-10 07:37:46 -0500

    Day 10 part 1 complete - thank you stack

--- src/2021/day10.js
+++ src/2021/day10.js
@@ -1,63 +1,71 @@
 function findIllegalClosureOnLine(input) {
     let inputArray = input.split("");
 
-    let parenCount = 0;
-    let bracketCount = 0;
-    let curlyBrace = 0;
-    let tagCount = 0;
-
-    console.log(inputArray);
+    let stack = [];
     for (let i = 0; i < inputArray.length; i++) {
         switch (inputArray[i]) {
             case '(':
-                parenCount++;
-                console.log("paren:", parenCount);
-                break;
             case '[':
-                bracketCount++;
-                console.log("bracket:", bracketCount);
-                break;
             case '{':
-                curlyBrace++;
-                console.log("curly:",curlyBrace);
-                break;
             case '<':
-                tagCount++;
-                console.log("tag:", tagCount);
+                stack.push(inputArray[i]);
                 break;
             case ')':
-                if (parenCount > 0) {
-                    parenCount--;
-                    console.log("paren:", parenCount);
-                } else {
-                    return ")";
-                }
-                break;
             case ']':
-                if (bracketCount > 0) {
-                    bracketCount--;
-                    console.log("bracket:", bracketCount);
-                } else {
-                    return "]";
-                }
-                break;
             case '}':
-                if (curlyBrace > 0) {
-                    curlyBrace--;
-                    console.log("curly:",curlyBrace);
-                } else {
-                    return "}";
-                }
-                break;
             case '>':
-                if (tagCount > 0) {
-                    tagCount--;
-                    console.log("tag:", tagCount);
-                } else {
-                    return ">";
+                if (isMatchingClosure(peak(stack), inputArray[i])) {
+                    stack.pop();
+                }
+                else {
+                    return inputArray[i];
                 }
                 break;
         }
     }
     return "";
+}
+
+//just for better readability
+function peak(array) {
+    return array[array.length - 1];
+}
+
+function isMatchingClosure(opening, closing) {
+    switch (opening) {
+        case '(':
+            return closing == ')';
+        case '[':
+            return closing == ']';
+        case '{':
+            return closing == '}';
+        case '<':
+            return closing == '>';
+    }
+}
+
+function scoreClosureErrors(input) {
+    let score = 0;
+    let temp;
+    for (let line = 0; line < input.length; line++) {
+        temp = findIllegalClosureOnLine(input[line]);
+        switch (temp) {
+            case ')':
+                score += 3;
+                break;
+            case ']':
+                score += 57;
+                break;
+            case '}':
+                score += 1197;
+                break;
+            case '>':
+                score += 25137;
+                break;
+                break;
+            default:
+            //do nothing
+        }
+    }
+    return score;
 }
