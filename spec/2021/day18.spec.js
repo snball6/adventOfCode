0a9201953b10bab91b97afe39cc0bdb76711d692
Sarah Ball <brooklyn@menloinnovations.com>
2021-12-24 10:57:51 -0500

    Day 18 - exploding and split pairs working. now to figure out the rest of split and put it together.

--- spec/2021/day18.spec.js
+++ spec/2021/day18.spec.js
@@ -1,7 +1,22 @@
 fdescribe("day18", () => {
     describe("Part 1", () => {
         it('should explode', () => {
-            expect(explode('[[[[[9,8],1],2],3],4]'.split(""))).toEqual('[[[[0,9],2],3],4]');
+                expect(explode('[[[[[9,8],1],2],3],4]'.split("")).join("")).toEqual('[[[[0,9],2],3],4]');
+                expect(explode('[7,[6,[5,[4,[3,2]]]]]'.split("")).join("")).toEqual('[7,[6,[5,[7,0]]]]');
+                expect(explode('[[6,[5,[4,[3,2]]]],1]' .split("")).join("")).toEqual('[[6,[5,[7,0]]],3]');
+                expect(explode('[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]'.split("")).join("")).toEqual('[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]');
+                expect(explode('[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]'.split("")).join("")).toEqual('[[3,[2,[8,0]]],[9,[5,[7,0]]]]');
+        });
+
+        it('should create split pair', () => {
+            expect(createSplitPair(10)).toEqual(['[',5,',',5,']']);
+            expect(createSplitPair(11)).toEqual(['[',6,',',5,']']);
+            expect(createSplitPair(12)).toEqual(['[',6,',',6,']']);
+        });
+
+        xit('should split', () => {
+            expect(split('[[[[0,7],4],[15,[0,13]]],[1,1]]'.split("")).join("")).toEqual('[[[[0,7],4],[[7,8],[0,13]]],[1,1]]');
+            expect(split('[[[[0,7],4],[[7,8],[0,13]]],[1,1]]'.split("")).join("")).toEqual('[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]');
         });
     });
 });
--- src/2021/day18.js
+++ src/2021/day18.js
@@ -1,14 +1,17 @@
+//it's big, it's not super elegant, but it gets the job done
 function explode(snailNumber) {
     let leftBracketCount = 0;
     let index = 0;
     while (leftBracketCount < 5) {
         if (snailNumber[index] == "[") {
             leftBracketCount++;
+        } else if (snailNumber[index] == "]") {
+            leftBracketCount--;
         }
         index++;
     }
-    let outerBracketIndex = index-1;
-    let rightBracketIndex = index+3;
+    let outerBracketIndex = index - 1;
+    let rightBracketIndex = index + 3;
 
     let pairLeft = parseInt(snailNumber[index]);
     let pairRightIndex = index + 2 //skipping a , 
@@ -39,13 +42,40 @@ function explode(snailNumber) {
             case ",":
                 break;
             default:
-                console.log("here?");
                 snailNumber[index] = parseInt(snailNumber[index]) + pairRight;
                 break outside2;
         }
         index++;
     }
-snailNumber.
-    console.log(snailNumber[index]);
-    return snailNumber.join("");
+    const beforePair = snailNumber.slice(0, outerBracketIndex);
+    const afterPair = snailNumber.slice(rightBracketIndex + 1);
+    let pairReplaced = beforePair.concat(0).concat(afterPair);
+    return pairReplaced;
+}
+
+function split(snailNumber) {
+    let index = 0;
+    while (index < snailNumber.length) {
+        switch (snailNumber[index]) {
+            case "[":
+            case "]":
+            case ",":
+                break;
+            default:
+                let number = parseInt(snailNumber[index]);
+                if (number >= 10) {
+
+                }
+        }
+        index++;
+    }
+}
+
+function createSplitPair(number) {
+    return ['[',
+        Math.ceil(number / 2)
+        , ',',
+        Math.floor(number / 2)
+        , ']'
+    ]
 }
