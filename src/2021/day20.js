9844019e17599e0fc07c7d9f376e1dd5ea69b1aa
Sarah Ball <brooklyn@menloinnovations.com>
2021-12-25 19:20:08 -0500

    Day 20 part 1 - the test samples work, but not the puzzle input : (

--- src/2021/day20.js
+++ src/2021/day20.js
@@ -55,12 +55,50 @@ class HelperImage {
     }
 }
 
-function calculatePixel(algorithm, surroundingString){
-    let binary = surroundingString.replaceAll('.','0').replaceAll('#','1');
+function calculatePixel(algorithm, surroundingString) {
+    let binary = surroundingString.replaceAll('.', '0').replaceAll('#', '1');
     let dec = convertBinaryToDec(binary); //day 16's parse int wrapper
     return algorithm[dec];
 }
 
-function countLit(algorithm, image, iterations){
-    return 0;
+function countLit(algorithm, img, iterations) {
+    let image = new HelperImage(img);
+    for (let iteration = 0; iteration < iterations; iteration++) {
+        let nextImage = new HelperImage([[]]);
+
+        for (let y = image.minY - 1; y <= image.maxY + 1; y++) {
+            for (let x = image.minX - 1; x <= image.maxX + 1; x++) {
+                //for each point with a buffer of one to allow for infinite size
+                let surrounding = image.getSurrounding(y, x);
+                nextImage.set(y, x, calculatePixel(algorithm, surrounding));
+            }
+        }
+        image = nextImage;
+        // consoleLogFriendlyDebug(image);
+
+    }
+
+    let litCounter = 0;
+    for (let y = image.minY; y <= image.maxY; y++) {
+        for (let x = image.minX; x <= image.maxX; x++) {
+            //for each point with a buffer of one to allow for infinite size
+            if (image.get(y, x) == '#') {
+                litCounter++;
+            };
+        }
+    }
+
+    return litCounter;
+}
+
+function consoleLogFriendlyDebug(image){
+    let arrayVersion = [];
+    for (let y = image.minY - 1; y < image.maxY + 1; y++) {
+        let row = '';
+        for (let x = image.minX - 1; x < image.maxX + 1; x++) {
+            row+=image.get(y, x);
+        }
+        arrayVersion.push(row);
+    }
+    console.log(arrayVersion);
 }
