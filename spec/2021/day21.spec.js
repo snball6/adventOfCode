cca8dd76d9518d319b3c855799df201d4ccbb72a
Sarah Ball <brooklyn@menloinnovations.com>
2021-12-28 14:46:08 -0500

    Day 21 - gets completely the wrong number but at least I'm not trapped in an infinite loop. Will need to do a detailed debug to figure out cause

--- spec/2021/day21.spec.js
+++ spec/2021/day21.spec.js
@@ -1,41 +1,49 @@
-describe("day21", () => {
-    describe("Part 1", () => {
-        it('should build a deterministic dice which loops through 100', () => {
-           let dice = new Dice();
-           expect(dice.getRollDeterministic()).toEqual(1);
-           expect(dice.getRollDeterministic()).toEqual(2);
-           for(let i = 0; i<97; i++){
-               dice.getRollDeterministic();
-           }
-           expect(dice.getRollDeterministic()).toEqual(100);
-           expect(dice.getRollDeterministic()).toEqual(1);
-           expect(dice.getRollDeterministic()).toEqual(2);
+fdescribe("day21", () => {
+   describe("Part 1", () => {
+      it('should build a deterministic dice which loops through 100', () => {
+         let dice = new Dice();
+         expect(dice.getRollDeterministic()).toEqual(1);
+         expect(dice.getRollDeterministic()).toEqual(2);
+         for (let i = 0; i < 97; i++) {
+            dice.getRollDeterministic();
+         }
+         expect(dice.getRollDeterministic()).toEqual(100);
+         expect(dice.getRollDeterministic()).toEqual(1);
+         expect(dice.getRollDeterministic()).toEqual(2);
 
-           expect(dice.incrementer).toEqual(102)
-        });
+         expect(dice.incrementer).toEqual(102)
+      });
 
-        it('should easily grab total of next 3', () => {
-            let dice = new Dice();
+      it('should easily grab total of next 3', () => {
+         let dice = new Dice();
 
-            expect(dice.getSumOfNext3Roll()).toEqual(1+2+3);
-            expect(dice.getSumOfNext3Roll()).toEqual(4+5+6);
-            expect(dice.getSumOfNext3Roll()).toEqual(7+8+9);
+         expect(dice.getSumOfNext3Roll()).toEqual(1 + 2 + 3);
+         expect(dice.getSumOfNext3Roll()).toEqual(4 + 5 + 6);
+         expect(dice.getSumOfNext3Roll()).toEqual(7 + 8 + 9);
 
-            expect(dice.incrementer).toEqual(9)
-         });
+         expect(dice.incrementer).toEqual(9)
+      });
 
-         it('should calculate the answer', () => {
-            expect(getPart1Answer(4,8)).toEqual(745*993);
-         });
+      it('should calculate the answer', () => {
+         expect(getPart1Answer(4, 8)).toEqual(745 * 993);
+      });
 
-         it('should calculate the answer', () => {
-            expect(getPart1Answer(7,2)).toEqual(678468);
-         });
+      it('should calculate the answer', () => {
+         expect(getPart1Answer(7, 2)).toEqual(678468);
+      });
 
-    });
-    describe("Part 2", () => {
+   });
+   describe("Part 2", () => {
+      it('should build a dice returning the sums each turn could produce - 3*3*3 options then repeats totaled', () => {
+         let dice = new QuantumDie();
+         //for example, the sum of 3 in one branch, 4 in 3, 5 in 6 cases, etc.
+         expect(dice.getSumOfNext3Roll()).toEqual({3: 1, 4: 3, 5: 6, 6: 7, 7: 6, 8: 3, 9: 1 });//7 possible each roll
+      });
 
-    });
+      it('should calculate the answer', () => {
+         expect(getPart2Answer(4, 8)).toEqual(444356092776315);
+      });
+   });
 
 
 });
--- src/2021/day21.js
+++ src/2021/day21.js
@@ -47,3 +47,127 @@ function getPart1Answer(player1Start, player2Start) {
         return player2Score * dice.incrementer;
     }
 }
+
+class QuantumDie {
+    constructor() {
+        this.options = {}
+        for (let roll1 = 1; roll1 < 4; roll1++) {
+            for (let roll2 = 1; roll2 < 4; roll2++) {
+                for (let roll3 = 1; roll3 < 4; roll3++) {
+                    addOrUpdate(this.options, roll1 + roll2 + roll3) //day 5 helper
+                }
+            }
+        }
+    }
+
+    getSumOfNext3Roll() {
+        return this.options;
+    }
+}
+
+function getPart2Answer(player1Start, player2Start) {
+    let dice = new QuantumDie();
+    let diceRolls = dice.getSumOfNext3Roll();
+
+    let games = [new Game(player1Start, player2Start, 0, 0, 1)];
+    let player1Wins = 0;
+    let player2Wins = 0;
+
+    while (games.length > 0) {
+        console.log("loop");
+        let tempGames1 = [];
+        //player 1 moves
+        for (let i = 0; i < games.length; i++) {
+            for (const roll in diceRolls) {
+                let currentGame = games[i];
+                let instancesOfRoll = diceRolls[roll];
+                // console.log('instances', instancesOfRoll);
+                // console.log('currentGame.instances', currentGame.instances);
+                // console.log('currentGame.instances * ', currentGame.instances* instancesOfRoll);
+                let player1Position = currentGame.player1Pos + roll;
+                player1Position = player1Position % 10 == 0 ? 10 : player1Position % 10; //there's probably a better way to do this, but lazy...
+                let player1Score = player1Position + currentGame.player1Score;
+                tempGames1.push(new Game(
+                    player1Position, //move 1
+                    currentGame.player2Pos, //2 stays same
+                    player1Score, //update 1 score
+                    currentGame.player2Score, //2 stays same
+                    currentGame.instances * instancesOfRoll //existing instances multiply by new branches
+                ));
+            }
+        }
+
+        let tempGames2 = [];
+        for (let i = 0; i < tempGames1.length; i++) {
+            if (tempGames1[i].findWinner()) {
+                // console.log("1 wins");
+                player1Wins += tempGames1[i].instances;
+                // console.log(player1Wins);
+            } else {
+                tempGames2.push(tempGames1[i]);
+            }
+        }
+
+        let tempGames3 = []
+        for (let i = 0; i < tempGames2.length; i++) {
+            for (const roll in diceRolls) {
+                let currentGame = tempGames2[i];
+                let instancesOfRoll = diceRolls[roll];
+                let player2Position = currentGame.player2Pos + roll;
+                player2Position = player2Position % 10 == 0 ? 10 : player2Position % 10; //there's probably a better way to do this, but lazy...
+                let player2Score = player2Position + currentGame.player2Score;
+                tempGames3.push(new Game(
+                    currentGame.player1Pos, //1 stays same
+                    player2Position, //2 moves
+                    currentGame.player1Score, // 1 stays same
+                    player2Score, //update 2 score
+                    currentGame.instances * instancesOfRoll //existing instances multiply by new branches
+                ));
+            }
+        }
+
+        games = [];
+        for (let i = 0; i < tempGames3.length; i++) {
+            if (tempGames3[i].findWinner()) {
+                player2Wins += tempGames3[i].instances;
+            } else {
+                games.push(tempGames3[i]);
+            }
+        }
+    }
+
+    return player1Wins > player2Wins ? player1Wins : player2Wins;
+
+}
+
+class Game {
+    constructor(player1Start, player2Start, player1Score, player2Score, instances) {
+        this.player1Pos = player1Start;
+        this.player2Pos = player2Start;
+        this.player1Score = player1Score;
+        this.player2Score = player2Score;
+        this.instances = instances; //how many games of this type?
+    }
+
+    checkForMerger(otherGame) {
+        if (otherGame.player1Pos == this.player1Pos &&
+            otherGame.player2Pos == this.player2Pos &&
+            otherGame.player1Score == this.player1Score &&
+            otherGame.player2Score == this.player2Score
+        ) {
+            return true;
+        } else {
+            false
+        }
+    }
+
+    findWinner() {
+        if (this.player1Score >= 21) {
+            return "player1";
+        }
+        if (this.player2Score >= 21) {
+            return "player2";
+        }
+        return null;
+    }
+}
