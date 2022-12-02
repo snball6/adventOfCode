0ce028b2ed281b49ca95a87fca0d7f084e7d9093
Sarah Ball <brooklyn@menloinnovations.com>
2022-12-02 18:27:53 -0500

    Refactor to see if I could skip the sort and evaluate for highest as I went.

--- src/2022/day1.js
+++ src/2022/day1.js
@@ -1,33 +1,38 @@
-function getHighestCalories(input){
+function getHighestCalories(input) {
     let highest = 0;
-    for(let i=0; i<input.length; i++){
-        let elfTotal =0;
-        for(let j=0; j<input[i].length; j++){
-            elfTotal+=input[i][j];
+    for (let i = 0; i < input.length; i++) {
+        let elfTotal = 0;
+        for (let j = 0; j < input[i].length; j++) {
+            elfTotal += input[i][j];
         }
-        if(elfTotal > highest){
+        if (elfTotal > highest) {
             highest = elfTotal;
         }
     }
     return highest;
 }
 
-function getTop3Highest(input){
-    let totals = [];
-
-    for(let i=0; i<input.length; i++){
-        let elfTotal =0;
-        for(let j=0; j<input[i].length; j++){
-            elfTotal+=input[i][j];
+function getTop3Highest(input) {
+    let highest = [0, 0, 0];
+    for (let i = 0; i < input.length; i++) {
+        let elfTotal = 0;
+        for (let j = 0; j < input[i].length; j++) {
+            elfTotal += input[i][j];
         }
-        totals.push(elfTotal);
+
+        if (elfTotal >= highest[0]) {
+            //new top
+            highest = [elfTotal, highest[0], highest[1]] //insert front, drop last
+        }
+        else if (elfTotal >= highest[1]) {
+            highest = [highest[0], elfTotal, highest[1]]; //insert second, drop last
+        }
+        else if (elfTotal > highest[2]) {
+            highest = [highest[0], highest[1], elfTotal]; //insert second, drop last
+        }
     }
 
-    totals.sort((a,b) => b-a);
-
-    let top3 = totals[0]+totals[1]+totals[2];
-
-    return top3;
+    return highest[0] + highest[1] + highest[2];
 }
 
 
