b889b5d58bcf4c66097d205132d4944af1a8895e
Sarah Ball <brooklyn@menloinnovations.com>
2022-12-02 08:01:24 -0500

    2022 setup prep

---
+++ spec/2022/day1.spec.js
@@ -0,0 +1,19 @@
+describe("day1", () => {
+
+    describe("Part 1", () => {
+        it("Should ", () => {
+
+            expect(5).toBe(7);
+        });
+
+    });
+
+    describe("Part 2", () => {
+        it("Should", () => {
+           
+        });
+
+      
+    });
+
+});
