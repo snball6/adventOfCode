97b93ed3f25e050c33c47e2973add555a964fe8a
Sarah Ball <brooklyn@menloinnovations.com>
2022-12-09 12:44:50 -0500

    Day 7 part 1 draft - works for sample set but too low of output for actual

--- src/2022/day7.js
+++ src/2022/day7.js
@@ -1,61 +1,98 @@
-let i = 0;
 function buildFileStructure(input) {
-    let i=0;
+    let i = 0;
+
     let root = {
         totalFileSize: 0,
         childFiles: [],
         childDirectories: []
     }
 
+    let directoryMap = {
+        'root': root
+    }
+
     let currentLocation = root;
     while (i < input.length) {
         if (input[i] == '$ ls') {
-            i++; //move to next - note based on spot check my puzzle input had no empty directories
-            while (!input[i].includes('$')) {
-                let line = input[i];
-                if (line[0] == 'd') {
-                    //add directory
-                    let directoryName = line.split(" ")[1];
-                    currentLocation.childDirectories.push(
-                        {
-                            [directoryName]: {
-                                totalFileSize: 0,
-                                childFiles: [],
-                                childDirectories: [],
-                                parent: currentLocation
-                            }
-                        }
-                    );
-                } else {
-                    //add file
-                    let fileName = line.split(" ")[1];
-                    let fileSize = parseInt(line.split(" ")[0])
-                    currentLocation.childFiles.push(
-                        {
-                            [fileName]: {
-                                totalFileSize: fileSize,
-                            }
-                        }
-                    );
-                    currentLocation.totalFileSize += fileSize;
-                }
-                i++
+            i++; //move forward to the list
+            //note based on spot check my puzzle input had no empty directories
+            i = ParseLSOutput(input, i, currentLocation, directoryMap);
+        } else {
+            //assume cd
+            let lineSplit = input[i].split(" ");
+            let directoryName = lineSplit[2]; // ['$', 'cd', 'dirname']
+            if (directoryName == "..") {
+                currentLocation = currentLocation.parent;
+            } else {
+                currentLocation = directoryMap[directoryName]
             }
-            //start adding objects to parent
-
-            break;
-
+            i++
         }
-        // if (input[i].includes('$ cd')) {
-        //     console.log("cd" + input[i]);
-        // }
-
-        // i++
     }
-    console.log(root);
-
+    return [root, directoryMap]; //lazy return 2 things...
 }
 
-function addSubFiles(structure, current){
-    
+function ParseLSOutput(input, i, currentLocation, directoryMap) {
+    while (!input[i].includes('$')) {
+        let line = input[i];
+        if (line[0] == 'd') {
+            //add directory
+            let directoryName = line.split(" ")[1];
+            let newDirectory = {
+                totalFileSize: 0,
+                childFiles: [],
+                childDirectories: [],
+                parent: currentLocation
+            }
+
+            currentLocation.childDirectories.push(
+                {
+                    [directoryName]: newDirectory
+                }
+            );
+
+            directoryMap[directoryName] = newDirectory;
+        } else {
+            //add file
+            let fileName = line.split(" ")[1];
+            let fileSize = parseInt(line.split(" ")[0]);
+            currentLocation.childFiles.push(
+                {
+                    [fileName]: {
+                        totalFileSize: fileSize,
+                    }
+                }
+            );
+            //recurse add file size to parents
+            let finished = addFileSizeToParents(currentLocation, fileSize);
+        }
+        i++;
+        if(i >= input.length){
+            //end of output
+            break;
+        }
+    }
+    return i;
+}
+
+function addFileSizeToParents(currentLocation, fileSize){
+    currentLocation.totalFileSize += fileSize;
+    //if it has a parent directory
+    if(currentLocation.parent){
+        addFileSizeToParents(currentLocation.parent, fileSize);
+    }
+    return true;
+}
+
+function partOneSolution(input){
+    let directories = buildFileStructure(input)[1];
+
+    let total = 0;
+    for(const [key, value] of Object.entries(directories)){
+        let current = directories[key];
+        if(current.totalFileSize <= 100000){
+            total+=current.totalFileSize;
+        }
+    }
+    return total;
 }
